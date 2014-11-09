c01ec449480de64fa71b59b776d637355d4a0f94
Martin Kelm <martinkelm@shrt.ws>
2014-11-09 16:26:46 +0100

    load and show map

--- tilemap/map.c
+++ tilemap/map.c
@@ -4,18 +4,47 @@
 extern SDL_Surface *screen;
 extern struct st_tile tiles[TILES_MAX];
 
+int map[MAP_MAX_Y][MAP_MAX_X];
+
 void map_init() {
   tiles_init();
+
+  FILE *fp;
+  if ((fp = fopen("map.dat", "r")) == NULL) {
+    fprintf(stderr, "Error while loading map data.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  int row = 0, col = 0;
+  char chunk[1024];
+  char *chunk_part;
+  while(fgets(chunk, 1024, fp) != NULL) {
+    col = 0;
+    chunk_part = strtok(chunk, " ");
+    while (chunk_part != NULL) {
+      map[row][col] = atoi(chunk_part);
+      chunk_part = strtok(NULL, " ");
+      col++;
+    }
+    row++;
+  }
+  fclose(fp);
+
 }
 
 void map_show() {
-  int i;
-  for (i = 0; i < TILES_MAX; i++) {
-    SDL_Rect offset;
-    offset.x = i * TILES_SIZE;
-    offset.y = 0;
-
-    SDL_BlitSurface(tiles[i].tile, NULL, screen, &offset);
+  SDL_Rect offset;
+  int row, col, x, y;
+  y = 0;
+  for (row = 0; row < MAP_MAX_Y; row++) {
+    x = 0;
+    for (col = 0; col < MAP_MAX_X; col++) {
+      offset.x = x;
+      offset.y = y;
+      SDL_BlitSurface(tiles[map[row][col]].tile, NULL, screen, &offset);
+      x += TILES_SIZE;
+    }
+    y += TILES_SIZE;
   }
 }
 
--- tilemap/map.h
+++ tilemap/map.h
@@ -1,3 +1,6 @@
+#define MAP_MAX_X 12
+#define MAP_MAX_Y 12
+
 void map_init();
 void map_show();
 void map_clean_up();
