0c215ad7562641ca877708ca49178067457d034b
Martin Kelm <martinkelm@shrt.ws>
2014-11-09 17:18:38 +0100

    improvement map loader without data

--- tilemap/map.c
+++ tilemap/map.c
@@ -1,10 +1,14 @@
 #include "map.h"
 #include "tiles.h"
 
+#define FALSE 0
+#define TRUE 1
+
 extern SDL_Surface *screen;
 extern struct st_tile tiles[TILES_MAX];
 
 int map[MAP_MAX_Y][MAP_MAX_X];
+int map_loaded = FALSE;
 
 SDL_Rect map_rect;
 SDL_Rect map_move_rect;
@@ -17,41 +21,40 @@ void map_init() {
   map_move_reset();
 
   FILE *fp;
-  if ((fp = fopen("map.dat", "r")) == NULL) {
-    fprintf(stderr, "Error while loading map data.\n");
-    exit(EXIT_FAILURE);
-  }
-
-  int row = 0, col = 0;
-  char chunk[1024];
-  char *chunk_part;
-  while(fgets(chunk, 1024, fp) != NULL) {
-    col = 0;
-    chunk_part = strtok(chunk, " ");
-    while (chunk_part != NULL) {
-      map[row][col] = atoi(chunk_part);
-      chunk_part = strtok(NULL, " ");
-      col++;
+  if ((fp = fopen("map.dat", "r")) != NULL) {
+    int row = 0, col = 0;
+    char chunk[1024];
+    char *chunk_part;
+    while(fgets(chunk, 1024, fp) != NULL) {
+      col = 0;
+      chunk_part = strtok(chunk, " ");
+      while (chunk_part != NULL) {
+        map[row][col] = atoi(chunk_part);
+        chunk_part = strtok(NULL, " ");
+        col++;
+      }
+      row++;
     }
-    row++;
+    fclose(fp);
+    map_loaded = TRUE;
   }
-  fclose(fp);
-
 }
 
 void map_show() {
-  SDL_Rect offset;
-  int row, col, x, y;
-  y = map_rect.y;
-  for (row = 0; row < MAP_MAX_Y; row++) {
-    x = map_rect.x;
-    for (col = 0; col < MAP_MAX_X; col++) {
-      offset.x = x;
-      offset.y = y;
-      SDL_BlitSurface(tiles[map[row][col]].tile, NULL, screen, &offset);
-      x += TILES_SIZE;
+  if (map_loaded == TRUE) {
+    SDL_Rect offset;
+    int row, col, x, y;
+    y = map_rect.y;
+    for (row = 0; row < MAP_MAX_Y; row++) {
+      x = map_rect.x;
+      for (col = 0; col < MAP_MAX_X; col++) {
+        offset.x = x;
+        offset.y = y;
+        SDL_BlitSurface(tiles[map[row][col]].tile, NULL, screen, &offset);
+        x += TILES_SIZE;
+      }
+      y += TILES_SIZE;
     }
-    y += TILES_SIZE;
   }
 }
 
